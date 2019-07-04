6943fbe7afd329b31916d03c7316b3c5840e12cd
minjib <48316078+minjib@users.noreply.github.com>
2019-07-05 01:34:30 +0900

    Update changecolor.py

--- changecolor.py
+++ changecolor.py
@@ -1,22 +1,33 @@
+#-*- coding: utf-8 -*-
+
 import cv2
 import numpy as np
+filename = raw_input("file name: ")
 print("H:0~180 S:0~255 V:0~255\n")
-print("hsv 시작 범위를 입력하시오.(작은 숫자 넣어야 함)\n")
+#print("hsv 시작 범위를 입력하시오.(작은 숫자 넣어야 함)\n")
+print("lower hsv value.(small number)\n")
 lH = input("H: ")
 lS = input("S: ")
 lV= input("V: ")
-print("hsv 끝 범위를 입력하시오.(큰 숫자 넣어야 함)\n")
+#print("hsv 끝 범위를 입력하시오.(큰 숫자 넣어야 함)\n")
+print("upper hsv value.(big number)\n")
 uH= input("H: ")
 uS= input("S: ")
 uV= input("V: ")
+#print("변환활 hsv 값을 입력하시오.(ex.0, -70, 80)\n")
+print("input hsv value to change.(eg.0, -70, 80)\n")
+cH=0
+cS=0
+cV=0
+cH= int(input("H: "))
+cS= int(input("S: "))
+cV= int(input("V: "))
 
-cap = cv2.VideoCapture('test.mp4')
+cap = cv2.VideoCapture(filename)
 
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
-out = cv2.VideoWriter('result.mp4',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
-
-
+out = cv2.VideoWriter('result.avi',cv2.VideoWriter_fourcc(*'DIVX'), 10, (frame_width,frame_height))
 
 while True:
     ret,frame = cap.read()
@@ -26,14 +37,17 @@ while True:
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_bound = np.array([161, 150, 75])
-    #lower_bound = np.array([lH, lS, lV])
-    upper_bound = np.array([174,255,255])
+    #lower_bound = np.array([6, 112, 41])
+    lower_bound = np.array([int(lH), int(lS), int(lV)])
+    #upper_bound = np.array([9,206,134])
+    upper_bound = np.array([int(uH), int(uS), int(uV)])
     mask_color = cv2.inRange(hsv, lower_bound, upper_bound)
     mask_inv = cv2.bitwise_not(mask_color)
     frame_replace = hsv
-    frame_replace[:,:,2] -= 75
-    frame_replace[:,:,1] -= 90;
+    frame_replace[:, :, 0] = frame_replace[:, :, 0]+ cH
+    frame_replace[:, :, 1] = frame_replace[:, :, 1]+ cS
+    frame_replace[:, :, 2] =frame_replace[:, :, 2]+ cV
+
     res = cv2.bitwise_and(frame_replace, frame_replace, mask=mask_color)
     res = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
 
